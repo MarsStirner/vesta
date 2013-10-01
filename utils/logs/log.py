dec20fd16de3cbf9a0cb405507a172b2ca6bc578
SAntipov <santipov@korusconsulting.ru>
2013-10-01 21:19:50 +0400

    Added logging

---
+++ utils/logs/log.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+import logging
+import urllib2
+import socket
+from utils.logs.pysimplelogs.pysimplelogs import Simplelog
+from config import SIMPLELOGS_URL, DEBUG, MODULE_NAME
+
+
+class SimplelogHandler(logging.Handler):
+
+    def __init__(self, url):
+        logging.Handler.__init__(self)
+        self.url = url
+
+    def emit(self, record):
+        message = self.format(record)
+        client = Simplelog(self.url)
+        level = logging.getLevelName(self.level).lower()
+        if not hasattr(client, level):
+            level = 'debug'
+        getattr(client, level)(MODULE_NAME, message, [MODULE_NAME])
+
+
+class VestaLogger(object):
+
+    @classmethod
+    def __check_url(cls, url):
+        try:
+            if urllib2.urlopen(url, timeout=2).getcode() == 200:
+                return True
+        except urllib2.URLError, e:
+            print e
+        except socket.timeout, e:
+            print e
+        return False
+
+    @classmethod
+    def __log_except(cls, logger):
+        #TODO: Нужно ли?! не будет ли зацикливания?
+        if not DEBUG:
+            # write to log all unhandled exceptions if not DEBUG mode
+            import sys
+            import traceback
+
+            def log_except_hook(*exc_info):
+                text = "".join(traceback.format_exception(*exc_info))
+                logger.error("Unhandled exception: %s", text)
+
+            sys.excepthook = log_except_hook
+
+    @classmethod
+    def get_logger(cls):
+        logger = logging.getLogger(MODULE_NAME)
+        logger.setLevel(logging.DEBUG)
+
+        if cls.__check_url(SIMPLELOGS_URL):
+            # create handler
+            handler = SimplelogHandler(SIMPLELOGS_URL)
+            # create formatter
+            formatter = logging.Formatter('%(asctime)s  - %(levelname)s - %(message)s')
+        else:
+            # create handler and set level to debug
+            handler = logging.StreamHandler()
+            # create formatter
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        # set handler level to debug
+        handler.setLevel(logging.DEBUG)
+        # add formatter to handler
+        handler.setFormatter(formatter)
+
+        cls.__log_except(logger)
+
+        return logger
