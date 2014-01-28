67e3ad073d3099b43be0b88a4c48c45083317b6f
SAntipov <santipov@korusconsulting.ru>
2014-01-28 22:09:04 +0400

    Link dicts

---
+++ app/lib/worker.py
@@ -0,0 +1,49 @@
+# -*- encoding: utf-8 -*-
+from app.lib.utils.tools import logger
+from .data import Dictionary, DictionaryNames, Collections
+
+
+class Worker:
+
+    def __init__(self, code):
+        self.code = code
+        self.collection = Dictionary(code)
+
+    def __link_document(self, document, data):
+        pass
+
+    def __prepare_value(self, value):
+        if isinstance(value, str) or isinstance(value, unicode):
+            value = value.replace('.', '')
+        return value
+
+    def __get_linked_data(self, code, field, value):
+        collection = Dictionary(code)
+        document = collection.get_document({field: self.__prepare_value(value)})
+        return document
+
+    def __link_documents(self, collection_code, origin_field, linked_field):
+        for document in self.collection.get_list():
+            if origin_field in document:
+                linked_document = self.__get_linked_data(collection_code, linked_field, document[origin_field])
+                if linked_document:
+                    document.update({collection_code: linked_document})
+                    self.collection.add_document(document)
+
+    def link_collection(self, collection_code, origin_field, linked_field):
+        dict_name = DictionaryNames()
+        origin_collection = dict_name.get_by_code(self.code)
+        linked_collection = dict_name.get_by_code(collection_code)
+        if not origin_collection:
+            logger.error(u'Не найден документ ({0}) в dict_names'.format(self.code), extra=dict(tags=['worker']))
+            return False
+        if not linked_collection:
+            logger.error(u'Не найден привязываемая коллекция ({0}) в dict_names'.format(collection_code),
+                         extra=dict(tags=['worker']))
+            return False
+        origin_collection.update({'linked': {'collection': linked_collection,
+                                             'origin_field': origin_field,
+                                             'linked_field': linked_field}})
+        dict_name.update(origin_collection.pop('_id'), origin_collection)
+
+        self.__link_documents(collection_code, origin_field, linked_field)
--- blueprints/admin/views/admin.py
+++ blueprints/admin/views/admin.py
@@ -8,6 +8,7 @@ from flask import flash, session
 from jinja2 import TemplateNotFound
 from ..app import module
 from app.lib.data import Collections, Dictionary, DictionaryNames
+from app.lib.worker import Worker
 
 
 @module.route('/')
@@ -20,19 +21,31 @@ def index():
         abort(404)
 
 
-@module.route('/dict_edit/<_id>/')
+@module.route('/dict_edit/<_id>/', methods=['GET', 'POST'])
 def dict_edit(_id):
     obj = DictionaryNames()
     info = obj.get_by_id(_id)
     collections = Collections()
     if not info or 'code' not in info or info['code'] not in collections.get_list():
         abort(404)
+
+    if request.method == 'POST':
+        worker = Worker(info['code'])
+        try:
+            worker.link_collection(request.values.get('linked_collection'),
+                                   request.values.get('origin_field'),
+                                   request.values.get('linked_field'))
+        except Exception, e:
+            flash(e, 'error')
     collection = Dictionary(info['code'])
-    # TODO: get fields for linked choose with other collection
     data = obj.get_list({'version': {'$exists': True}})
     fields = _get_fields(info['code'])
     try:
-        return render_template('{0}/dict_edit.html'.format(module.name), info=info, data=data, fields=fields)
+        return render_template('{0}/dict_edit.html'.format(module.name),
+                               info=info,
+                               data=data,
+                               fields=fields,
+                               documents=collection.get_list())
     except TemplateNotFound:
         abort(404)
 
@@ -45,7 +58,6 @@ def dict_view(_id):
     if not info or 'code' not in info or info['code'] not in collections.get_list():
         abort(404)
     collection = Dictionary(info['code'])
-    # TODO: get fields for linked choose with other collection
     data = obj.get_list({'version': {'$exists': True}})
     fields = _get_fields(info['code'])
     try:
@@ -54,14 +66,16 @@ def dict_view(_id):
         abort(404)
 
 
-@module.route('/ajax_fields/<_id>')
-def get_fields(_id):
-    pass
+@module.route('/ajax_fields/')
+@module.route('/ajax_fields/<code>')
+def get_fields(code=None):
+    if code:
+        return jsonify(result=_get_fields(code))
 
 
 def _get_fields(code):
     obj = Dictionary(code)
     document = list(obj.get_list(limit=1))
     if document[0]:
-        return document[0].keys()
+        return sorted(document[0].keys())
     return []
