89404ff8c84460aab2a6c7d4aa3458d94d01460a
SAntipov <santipov@korusconsulting.ru>
2014-01-29 21:58:54 +0400

    Getting data

--- app/app.py
+++ app/app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 app.config.from_object(config)
 
 from blueprints.admin.app import module as admin
-# from blueprints.api.app import module as api
+from blueprints.api.app import module as api
 # from blueprints.soap.app import module as soap
 
 app.register_blueprint(admin, url_prefix='/{0}'.format(admin.name))
-# app.register_blueprint(api, url_prefix='/{0}'.format(api.name))
+app.register_blueprint(api, url_prefix='/{0}'.format(api.name))
 # app.register_blueprint(soap, url_prefix='/{0}'.format(soap.name))
 
 # Import all views
--- blueprints/api/views/rest.py
+++ blueprints/api/views/rest.py
@@ -5,6 +5,7 @@ from app.lib.utils.exceptions import InvalidAPIUsage
 from app.lib.data import Clients, DictionaryNames, Dictionary
 from app.lib.utils.tools import jsonify as vesta_jsonify, json
 from bson.objectid import InvalidId
+from ..app import module
 
 
 def user_required(f):
@@ -267,3 +268,24 @@ class DictionaryAPI(MethodView, APIMixin):
         mod.add_url_rule(url, view_func=f, methods=['GET'], defaults={'document_id': None})
         mod.add_url_rule(url, view_func=f, methods=['POST'])
         mod.add_url_rule('{0}<string:document_id>/'.format(url), view_func=f, methods=['GET', 'PUT', 'DELETE'])
+
+
+@module.route('/<code>/<field>/<doc_code>/nsi/')
+def get_linked_data(code, field, doc_code):
+    # TODO: try-except
+    obj = Dictionary(code)
+    obj_names = DictionaryNames()
+    try:
+        result = obj.get_document({str(field): doc_code})
+        if not result:
+            return make_response(vesta_jsonify(), 200)
+        origin_dict = obj_names.get_by_code(code)
+        try:
+            linked_dict = origin_dict['linked']['collection']
+        except AttributeError:
+            raise InvalidAPIUsage(u'Not found', status_code=404)
+    except TypeError, e:
+        raise InvalidAPIUsage(e.message, status_code=400)
+    except InvalidId, e:
+        raise InvalidAPIUsage(e.message, status_code=404)
+    return make_response(vesta_jsonify(dict(oid=linked_dict['oid'], data=result[linked_dict['code']])), 200)
