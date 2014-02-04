a9441656fb65ae9b4f2ad672c5e639f1d4eb71b2
SAntipov <santipov@korusconsulting.ru>
2014-02-04 21:38:08 +0400

    establish one connection per request/script

--- app/connectors.py
+++ app/connectors.py
@@ -2,11 +2,15 @@
 from pymongo import MongoClient
 from config import MONGODB_CONNECT_URI, MONGODB_DB, MONGODB_USER, MONGODB_PASSWORD, MONGODB_HOST
 
+connections = dict()
+
 
 class MongoConnection:
     @classmethod
     def provider(cls, db_name=None, db_type='local'):
         connection, db = None, None
+        if db_type in connections:
+            return connections[db_type]
         if db_type == 'local':
             connection = MongoClient(MONGODB_HOST, tz_aware=True)
             if db_name is None:
@@ -14,4 +18,5 @@ class MongoConnection:
             if MONGODB_USER and MONGODB_PASSWORD:
                 connection[db_name].authenticate(MONGODB_USER, MONGODB_PASSWORD, source=db_name)
             db = connection[db_name]
+            connections[db_type] = connection, db
         return connection, db
