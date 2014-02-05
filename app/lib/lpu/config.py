16dde082b5c5f1d1d9970fd1eca40d1c6e1bd41b
root <root@ubuntu.(none)>
2014-02-05 19:50:54 +0400

    Дамп сопопставленных справочников для Пензы

--- app/lib/lpu/config.py
+++ app/lib/lpu/config.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 DB_HOST = 'localhost'
 DB_PORT = 3306
-DB_USER = 'root'
+DB_USER = 'tmis'
 DB_PASSWORD = 'q1w2e3r4t5'
-DB_NAME = 'ntk_reference_162'
+DB_NAME = 's11r64'
 
 DB_CONNECT_STRING = 'mysql://%s:%s@%s:%s/%s?charset=utf8' % (DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
 
 refbooks = ['rbAcheResult',
             'rbDocumentType',
             'rbFinance',
-            'rbOKVED',
+#            'rbOKVED',
             'rbPost',
             'rbResult',
             'rbSocStatusClassTypeAssoc',
             'rbSocStatusType',
             'rbSpeciality',
-            'Organisation',
+#            'Organisation',
             'rbTraumaType']
--- scripts/nsi_import.py
+++ scripts/nsi_import.py
@@ -6,5 +6,5 @@ sys.path.insert(0, os.path.dirname('..'))
 from app.lib.nsi.data import NSI_Data
 
 obj = NSI_Data()
-# obj.import_nsi_dictionaries(['KLD116'])
-obj.import_nsi_dictionaries(overwrite=True)
+obj.import_nsi_dictionaries(['KLD116'], overwrite=True)
+#obj.import_nsi_dictionaries()
