cad5e8ccdb64e5f1be009453b2e9b58bbf89d5d6
SAntipov <santipov@korusconsulting.ru>
2014-04-16 13:09:44 +0400

    Excluded rb_*

--- app/lib/lpu/db.py
+++ app/lib/lpu/db.py
@@ -5,7 +5,7 @@ from .config import DB_CONNECT_STRING
 
 engine = create_engine(DB_CONNECT_STRING, convert_unicode=True, pool_recycle=600)
 meta = MetaData(engine)
-meta.reflect(views=True, only=lambda name, obj: 'rb' in name)
+meta.reflect(views=True, only=lambda name, obj: 'rb' in name and not 'rb_' in name)
 connection = engine.connect()
 
 
--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'0.2.10'
+version = u'0.2.11'
 last_change_date = datetime(year=2014, month=4, day=16, hour=13,)
