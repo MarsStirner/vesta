5be1a61f47996568c0e406304aa7c38138420e3e
SAntipov <santipov@korusconsulting.ru>
2014-05-28 18:12:01 +0400

    Выпилил pysymplelogs из git-подмодулей и добавил зависимостью

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'0.2.12'
-last_change_date = datetime(year=2014, month=4, day=16, hour=17,)
+version = u'0.2.13'
+last_change_date = datetime(year=2014, month=5, day=28, hour=17,)
