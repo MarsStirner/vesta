75ce9aa887fac74bb5a0e4be3538e37066749ca7
SAntipov <santipov@korusconsulting.ru>
2014-09-18 12:36:05 +0400

    bugfix-TMIS-356

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'0.2.22'
-last_change_date = datetime(year=2014, month=6, day=25, hour=15,)
+version = u'0.2.23'
+last_change_date = datetime(year=2014, month=9, day=18, hour=12,)
